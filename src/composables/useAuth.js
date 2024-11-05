e6fce01cbf801b2f55b7f06815ea949479f55476
Azenga Kevin <azenga.kevin7@gmail.com>
2024-11-05 15:36:52 +0300

    wip: authentication pages update

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -3,13 +3,14 @@ import axiosInstance from "@/axiosconfig/axiosInstance";
 import { useRouter } from "vue-router";
 import { useLocalStorage } from "@vueuse/core";
 
-const useAuth = () => {
-  const username = ref("");
+export default function useAuth() {
+  const name = ref("");
   const email = ref("");
   const password = ref("");
   const router = useRouter();
 
   const accessToken = useLocalStorage("access-token", null);
+
   const refreshToken = useLocalStorage("refresh-token", null);
 
   const login = async () => {
@@ -22,19 +23,47 @@ const useAuth = () => {
       accessToken.value = response.data.access_token;
       refreshToken.value = response.data.refresh_token;
 
-      router.push("/dashboard"); // Correctly navigating to the dashboard
+      router.push("/dashboard");
     } catch (error) {
       console.error(error);
-      alert("Login failed. Please try again.");
+    }
+  };
+
+  const signup = async () => {
+    try {
+      const response = await axiosInstance.post("api/register/", {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+      });
+
+      accessToken.value = response.data.access_token;
+      refreshToken.value = response.data.refresh_token;
+
+      router.push("/dashboard");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await axiosInstance.post("api/logout/", {
+        refresh: accessToken.value,
+      });
+
+      router.push("/login");
+    } catch (error) {
+      console.error(error);
     }
   };
 
   return {
-    username,
+    name,
     email,
     password,
     login,
+    signup,
+    logout,
   };
-};
-
-export default useAuth;
+}
